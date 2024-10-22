11d1df2d1ecf4aeb64d5ade4b96ee65a52bf2767
Jason Ericson <jasonmarkericson@gmail.com>
2024-10-22 17:58:22 +0000

    Added avatar publishing

--- publishfeed.py
+++ publishfeed.py
@@ -9,10 +9,17 @@ def main():
 
     feed_did = f'did:web:{config.HOSTNAME}'
 
+    avatar_blob = None
+    avatar_path = feed_config.get('avatar_path', None)
+    if avatar_path is not None and avatar_path != '':
+        with open(avatar_path, 'rb') as file:
+            avatar_data = file.read()
+            avatar_blob = client.com.atproto.repo.upload_blob(avatar_data).blob
+
     # response = client.com.atproto.repo.delete_record(models.ComAtprotoRepoDeleteRecord.Data(
     #     repo=client.me.did,
     #     collection=models.ids.AppBskyFeedGenerator,
-    #     rkey=RECORD_NAME,
+    #     rkey='favorites',
     # ))
     response = client.com.atproto.repo.put_record(models.ComAtprotoRepoPutRecord.Data(
         repo=client.me.did,
@@ -22,13 +29,13 @@ def main():
             did=feed_did,
             display_name=feed_config['display_name'],
             description=feed_config['description'],
-            avatar=None,
+            avatar=avatar_blob,
             created_at=client.get_current_time_iso(),
         )
     ))
 
     print('Successfully published!')
-    print('Feed URI:', response.uri)
+    # print('Feed URI:', response.uri)
 
 if __name__ == '__main__':
     main()
